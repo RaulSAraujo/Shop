f993910c3cb94cfa9706dfc6312f968780f9903e
Raul Silva Araujo <raul.araujo.2000@hotmail.com>
2022-05-18 09:04:56 -0300

    Marcando como favorito

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -13,30 +15,32 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        fontFamily: 'Lato',
-        colorScheme: const ColorScheme(
-          brightness: Brightness.light,
-          primary: Colors.purple,
-          onPrimary: Colors.white,
-          secondary: Colors.cyan,
-          onSecondary: Colors.black,
-          error: Colors.red,
-          onError: Colors.red,
-          background: Colors.white,
-          onBackground: Colors.white30,
-          onSurface: Colors.black,
-          surface: Colors.black,
-        ),
-      ),
-      home: ProductsOverviewPage(),
-      debugShowCheckedModeBanner: false,
-      routes: {
-        AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage(),
-      }
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            fontFamily: 'Lato',
+            colorScheme: const ColorScheme(
+              brightness: Brightness.light,
+              primary: Colors.purple,
+              onPrimary: Colors.white,
+              secondary: Colors.cyan,
+              onSecondary: Colors.black,
+              error: Colors.red,
+              onError: Colors.red,
+              background: Colors.white,
+              onBackground: Colors.white30,
+              onSurface: Colors.black,
+              surface: Colors.black,
+            ),
+          ),
+          home: ProductsOverviewPage(),
+          debugShowCheckedModeBanner: false,
+          routes: {
+            AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage(),
+          }),
     );
   }
 }
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:shop/components/product_item.dart';
-import 'package:shop/data/dummy_data.dart';
-import 'package:shop/models/product.dart';
+
+import '../components/product_grid.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
-  final List<Product> loadedProducts = dummyProducts;
   ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
@@ -14,17 +12,7 @@ class ProductsOverviewPage extends StatelessWidget {
         title: const Text('Minha Loja'),
         centerTitle: true,
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3/2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemCount: loadedProducts.length,
-        itemBuilder: (ctx,i)=> ProductItem(product: loadedProducts[i]),
-      ),
+      body: ProductGrid(),
     );
   }
 }
